e84dc875e9e76befa5bef34f3fb11f503035510e
Yiluan Song <yiluansong@Yiluans-MacBook-Air.local>
2025-07-13 20:52:37 -0400

    direct export

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { Redirect } from 'expo-router';
+import CalendarPage from './calendar';
 
 export default function Index() {
-  return <Redirect href="/calendar" />;
+  return <CalendarPage />;
 } 
